635bd419156bdaa2242faf41343329fd0ee0e232
Christopher Njihia <crnjihia@gmail.com>
2024-11-22 20:59:51 +0300

    Second Commit

--- 103-merge_sort.c
+++ 103-merge_sort.c
@@ -8,17 +8,21 @@
  */
 void *_calloc(unsigned int nmemb, unsigned int size)
 {
-	unsigned int i = 0;
-	char *p;
+    unsigned int i = 0;
+    char *p;
 
-	if (nmemb == 0 || size == 0)
-		return ('\0');
-	p = malloc(nmemb * size);
-	if (p == '\0')
-		return ('\0');
-	for (i = 0; i < (nmemb * size); i++)
-		p[i] = '\0';
-	return (p);
+    if (nmemb == 0 || size == 0)
+        return (NULL);  // Use NULL instead of '\0'
+    
+    p = malloc(nmemb * size);
+    
+    if (p == NULL)  // Check for NULL instead of '\0'
+        return (NULL);
+    
+    for (i = 0; i < (nmemb * size); i++)
+        p[i] = '\0';
+    
+    return (p);
 }
 /**
  *merge - Do a merge to join
